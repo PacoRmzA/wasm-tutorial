218f403d3b610122ad7f6be5d290e97e02639c95
PacoRmzA <a01634262@itesm.mx>
2022-09-19 23:57:55 -0500

    Tutorial 4 (memory)

---
+++ memory/malloc.c
@@ -0,0 +1,30 @@
+#include <emscripten.h>
+#include <stdlib.h>
+
+int main() {
+    return 0;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int accumulate(int *array, int n) {
+    int sum = 0;
+    while (n) {
+        sum += array[--n];
+    }
+    return sum;
+}
+
+EMSCRIPTEN_KEEPALIVE
+const char *getString() {
+    return "Hello, world!";
+}
+
+EMSCRIPTEN_KEEPALIVE
+void *wasmmalloc(size_t n) {
+    return malloc(n);
+}
+
+EMSCRIPTEN_KEEPALIVE
+void wasmfree(void *ptr) {
+    free(ptr);
+}
